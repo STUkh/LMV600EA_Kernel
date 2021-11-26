f3f2a632af8a8771ee4b3682cba3f39d25617b08
ErickG233 <1529896768@qq.com>
2021-11-26 13:51:43 +0800

    devfreq_boost: adapt to LGE panel notifier
    from dreamisbaka <jolinux.g@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -6,7 +6,7 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/msm_drm_notify.h>
+#include <linux/lge_panel_notify.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
@@ -30,7 +30,7 @@ struct boost_dev {
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block msm_drm_notif;
+	struct notifier_block lge_panel_notif;
 };
 
 static void devfreq_input_unboost(struct work_struct *work);
@@ -179,23 +179,23 @@ static int devfreq_boost_thread(void *data)
 	return 0;
 }
 
-static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
+static int lge_panel_notifier_cb(struct notifier_block *nb, unsigned long action,
 			  void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
+	struct df_boost_drv *d = container_of(nb, typeof(*d), lge_panel_notif);
 	int i;
-	struct msm_drm_notifier *evdata = data;
-	int *blank = evdata->data;
+	struct lge_panel_notifier *panel_data = data;
+	int *blank = &(panel_data->state);
 
 	/* Parse framebuffer blank events as soon as they occur */
-	if (action != MSM_DRM_EARLY_EVENT_BLANK)
+	if (action == LGE_PANEL_EVENT_BLANK && *blank != LGE_PANEL_STATE_BLANK)
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		if (*blank == MSM_DRM_BLANK_UNBLANK) {
+		if (*blank != LGE_PANEL_STATE_BLANK) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
@@ -317,9 +317,9 @@ static int __init devfreq_boost_init(void)
 		goto stop_kthreads;
 	}
 
-	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
-	d->msm_drm_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&d->msm_drm_notif);
+	d->lge_panel_notif.notifier_call = lge_panel_notifier_cb;
+	d->lge_panel_notif.priority = INT_MAX;
+	ret = lge_panel_notifier_register_client(&d->lge_panel_notif);
 	if (ret) {
 		pr_err("Failed to register fb notifier, err: %d\n", ret);
 		goto unregister_handler;
