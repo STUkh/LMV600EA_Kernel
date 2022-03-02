dc0060482599b14de800537cd44fea178c5c5864
EmanuelCN <emanuelghub@gmail.com>
2022-03-02 18:08:28 +0800

    kernel: Boost CPU at zygote-forks

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/scs.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2370,6 +2371,7 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 150 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(150);
 		devfreq_boost_kick_max(DEVFREQ_CPU_LLCC_DDR_BW, 150);
 	}
 
