afd90041d5945f84fde88f08a5bf1634e622b27c
ErickG233 <1529896768@qq.com>
2022-03-21 02:34:08 +0800

    mm: compaction.c: Use LGE panel notify
    from Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- mm/compaction.c
+++ mm/compaction.c
@@ -23,7 +23,7 @@
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
 #include <linux/psi.h>
-#include <linux/msm_drm_notify.h>
+#include <linux/lge_panel_notify.h>
 #include <linux/moduleparam.h>
 #include <linux/time.h>
 #include <linux/workqueue.h>
@@ -2422,37 +2422,41 @@ module_param_named(compaction_screen_off_delay_ms,
 static unsigned long compaction_forced_timeout;
 
 
-static int msm_drm_notifier_callback(struct notifier_block *self,
+static int lge_panel_notifier_callback(struct notifier_block *self,
 				       unsigned long event, void *data)
 {
-	struct msm_drm_notifier *evdata = data;
-	int *blank;
+	struct lge_panel_notifier *panel_data = data;
 
-	if (event != MSM_DRM_EVENT_BLANK && event != MSM_DRM_EARLY_EVENT_BLANK)
-		goto out;
-
-	if (!evdata || !evdata->data)
-		goto out;
-
-	blank = evdata->data;
-	switch (*blank) {
-	case MSM_DRM_BLANK_POWERDOWN_CUST:
-	case MSM_DRM_BLANK_POWERDOWN:
-	case MSM_DRM_BLANK_NORMAL:
-		if (!screen_on)
+	switch (event) {
+		case LGE_PANEL_EVENT_RESET:
 			goto out;
-		screen_on = false;
-		if (time_after(jiffies, compaction_forced_timeout) && !delayed_work_busy(&compaction_work)) {
-			compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
-			queue_delayed_work(compaction_wq, &compaction_work,
-					   msecs_to_jiffies(compaction_soff_delay_ms));
-		}
-		break;
-	case MSM_DRM_BLANK_UNBLANK_CUST:
-		if (screen_on)
+			break;
+		case LGE_PANEL_EVENT_POWER:
 			goto out;
-		screen_on = true;
-		break;
+			break;
+		case LGE_PANEL_EVENT_RECOVERY:
+			goto out;
+			break;	
+		case LGE_PANEL_EVENT_DUAL_DISPLAY:
+			goto out;
+			break;	
+		case LGE_PANEL_EVENT_BLANK:
+			if (panel_data->state == LGE_PANEL_STATE_UNBLANK) {
+				if (screen_on)
+					goto out;
+				screen_on = true;
+			} else if (panel_data->state == LGE_PANEL_STATE_BLANK) {
+				if (!screen_on)
+					goto out;
+				screen_on = false;
+				compaction_forced_timeout = jiffies + msecs_to_jiffies(compaction_timeout_ms);
+				queue_delayed_work(compaction_wq, &compaction_work,
+						msecs_to_jiffies(compaction_soff_delay_ms));
+			}
+			break;
+		default:
+			goto out;
+			break;
 	}
 
 out:
@@ -2460,7 +2464,7 @@ out:
 }
 
 static struct notifier_block compaction_notifier_block = {
-	.notifier_call = msm_drm_notifier_callback,
+	.notifier_call = lge_panel_notifier_callback,
 };
 
 /* Compact all zones within a node */
@@ -2820,8 +2824,6 @@ static int __init kcompactd_init(void)
 }
 subsys_initcall(kcompactd_init)
 
-extern struct drm_panel *lcd_active_panel;
-
 static int  __init scheduled_compaction_init(void)
 {
 	compaction_wq = create_freezable_workqueue("compaction_wq");
@@ -2831,10 +2833,7 @@ static int  __init scheduled_compaction_init(void)
 
 	INIT_DELAYED_WORK(&compaction_work, do_compaction);
 
-	if (lcd_active_panel) {
-		drm_panel_notifier_register(lcd_active_panel,
-					    &compaction_notifier_block);
-	}
+	lge_panel_notifier_register_client(&compaction_notifier_block);
 
 	return 0;
 }
