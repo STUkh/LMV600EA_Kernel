036b1314dcfedd852da6ed6f6e5f328772795218
Tim Froidcoeur <tim.froidcoeur@tessares.net>
2022-07-11 16:39:21 +0200

    mptcp: fix accounting on rmem
    Linked to previous commit: "mptcp: fix tcp-mem accounting"
    
    rmem accounting is always done on the meta but tcp_mem (sk_rmem_schedule
    call) is done on the individual subflows.
    
    Properly split between meta and subflow in tcp_try_rmem_schedule.
    
    Fixes: c50cf089f03b3 ("Fix: mptcp_try_rmem_schedule removed")
    Signed-off-by: Nathan Atta <nathan.atta@tessares.net>
    Signed-off-by: Tim Froidcoeur <tim.froidcoeur@tessares.net>
    Acked-by: Christoph Paasch <cpaasch@apple.com>
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 0bdffb33af4f94545c454786e2b7c0bd9e91acb4)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>
    (cherry picked from commit 7424545a1d576151475cca16cf7de5047774fef0)
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4666,18 +4666,16 @@ static int tcp_prune_queue(struct sock *sk);
 static int tcp_try_rmem_schedule(struct sock *sk, struct sk_buff *skb,
 				 unsigned int size)
 {
-#ifdef CONFIG_MPTCP
-	if (mptcp(tcp_sk(sk)))
-		sk = mptcp_meta_sk(sk);
-#endif
-	if (atomic_read(&sk->sk_rmem_alloc) > sk->sk_rcvbuf ||
+	struct sock *meta_sk = mptcp(tcp_sk(sk)) ? mptcp_meta_sk(sk) : sk;
+
+	if (atomic_read(&meta_sk->sk_rmem_alloc) > meta_sk->sk_rcvbuf ||
 	    !sk_rmem_schedule(sk, skb, size)) {
 
-		if (tcp_prune_queue(sk) < 0)
+		if (tcp_prune_queue(meta_sk) < 0)
 			return -1;
 
 		while (!sk_rmem_schedule(sk, skb, size)) {
-			if (!tcp_prune_ofo_queue(sk))
+			if (!tcp_prune_ofo_queue(meta_sk))
 				return -1;
 		}
 	}
